310884da06f529655db0f7ef9ef56c4826ba3555
jcassidy91 <jamie@Lambda-School-Jamies-MacBook-Pro.local>
2022-07-15 15:06:59 -0700

    initial

--- src/com/bloomtech/dependencyinjection/remotecontroller/models/GargageDoor.java
+++ src/com/bloomtech/dependencyinjection/remotecontroller/models/GargageDoor.java
@@ -1,10 +1,20 @@
 package com.bloomtech.dependencyinjection.remotecontroller.models;
 
-public class GargageDoor {
+public class GargageDoor implements IRemote {
     private boolean open;
 
     public GargageDoor() {
         open = false;
     }
 
+    @Override
+    public void onPower() {
+        open = !open;
+        //Turn on light
+    }
+
+    @Override
+    public void onAdjuster1(boolean up) {
+        //No implementation
+    }
 }
---
+++ src/com/bloomtech/dependencyinjection/remotecontroller/models/IRemote.java
@@ -0,0 +1,6 @@
+package com.bloomtech.dependencyinjection.remotecontroller.models;
+
+public interface IRemote {
+    void onPower();
+    void onAdjuster1(boolean up);
+}
